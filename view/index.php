cb0093dfc94e7496f51bfee21577ec6930ad1931
Fabiano Monteiro <fabianodesenvolvedor@outlook.com>
2015-04-11 18:47:21 -0300

    alteracao no index.php

--- view/index.php
+++ view/index.php
@@ -5,5 +5,12 @@
       <div class="col-xs-1">.col-md-1</div>
       <div class="col-xs-1">.col-md-1</div>
       <div class="col-xs-1">.col-md-1</div>
-    </div>
+    </div>  
+      <div class="panel panel-default">
+  <div class="panel-heading">Panel heading without title</div>
+  <div class="panel-body">
+    Panel content
+  </div>
+</div>
+    
 
